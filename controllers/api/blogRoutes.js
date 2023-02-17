51d1d33df641354411e1c5d780c161c243916add
Analise <analise.giobbi@gc.com>
2023-02-17 14:51:09 -0500

    added home routes and blog routes

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,53 @@
+const router = rquire('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auths');
+
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const newBlogPost = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+        res.status(200).json(newBlogPost)
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//come back to this route
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.update(
+            {
+                user_id: req.body.user_id,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            },
+        )
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try{
+        const blogPostData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.params.user_id,
+            },
+        });
+        if (!blogPostData){
+            res.status(404).json({ message: 'There are no posts wiht this id' })
+            return;
+        }
+        res.status(200).json(blogPostData);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
-router = require('express').Router();
+const router = require('express').Router();
+const { route } = require('.');
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auths')
 
@@ -8,7 +9,7 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ['username'],
                 },
             ],
         });
@@ -20,6 +21,53 @@ router.get('/', async (req, res) => {
     }catch(err){
         res.status(500).json(err)
     }
+});
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
+
+router.get('/home', withAuth, async (req, res) => {
+    try{
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+        const user = userData.get({ plain: true });
+        res.render('home', {
+            ...user,
+            logged_in: true
+        });
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.logged_in) {
+        res.redirect('/home');
+        return;
+    }
+    res.render('login')
+})
+
+module.exports = router;
 
 
