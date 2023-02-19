d00901f93c865e33d18ccd77b01ac964dd0899b8
Analise <analise.giobbi@gc.com>
2023-02-19 12:44:53 -0500

    attempt to add update and delete to user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,48 @@
-router = require('express').Router();
-const { User } = require('../../models');
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../../models');
+const session = require('express-session')
+const withAuth = require('../../utils/auths')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+router.get('/', async (req, res) => {
+    try{
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] }
+        })
+        res.status(200).json(userData)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+router.get('/:id', async (req, res) =>{
+    try{
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Blog,
+                    attributes: ['id', 'title', 'content', 'created_at'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_content', 'blog_id', 'user_id', 'create_at'],
+                    include: {
+                        model: Blog,
+                        attributes: ['title'],
+                    }
+                },
+            ],
+        })
+        if (!userData){
+            res.status(404).json({ message: 'no users with this id' })
+        }
+        res.status(200).json(userData)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 router.post('/', async (req, res) =>{
     try {
@@ -40,6 +83,35 @@ router.post('/login', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.put('/:id', withAuth, async (req, res) =>{
+    try{
+        const userData = await User.update(req.body, {
+            individualHooks: true,
+            where: {
+                id: req.params.id,
+            }
+        })
+        if(!userData){
+            res.status(404).json({ message: "A user with this id does not exist" })
+        }
+        res.status(200).json(userData)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) =>{
+    try{
+        const userData = await User.destroy(req.params.id)
+        if(!userData){
+            res.status(404).json({ message: "A user with this id does not exist" })
+        }
+        res.status(200).json(userData)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 router.post('/logout', (req, res) =>{
     if (req.session.logged_in) {
