890534f22fb347f6a0bf2125c4fa8dcf9a72453b
Analise <analise.giobbi@gc.com>
2023-02-20 10:47:31 -0500

    update to login and signup forms

--- public/js/login.js
+++ public/js/login.js
@@ -18,27 +18,5 @@ const loginFormHandler = async (event) =>{
     }
 };
 
-
-const signupFormHandler = async (event) =>{
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            header: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/home');
-        }else{
-            alert(response.statusText);
-        }
-    }
-};
-
 document.querySelector('.login-form').addEventListener('submin', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 
